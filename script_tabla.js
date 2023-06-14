7f84c1ab315baf9670a993072c450fc976095b78
olinicholson <olinicholson4@gmail.com>
2023-06-14 12:04:45 -0300

    canciones

--- script_tabla.js
+++ script_tabla.js
@@ -1,14 +1,35 @@
-const tableDiv = containerr.append("div").style("flex-grow", 1);
+d3.csv('datos/datos_merge.csv', d3.autoType).then(data => {
+  data.forEach(d => {
+    d.msPlayed = d.msPlayed / (1000 * 60 * 60); // Convertir los milisegundos a horas
+  });
 
-/* Crear tabla en el HTML */
-const table = tableDiv.append("table").style("margin", "auto");
+  const artistCounts = d3.rollup(
+    data,
+    v => new Set(v.map(d => d.artistName)).size,
+    d => d.Nombre
+  );
+  const songCounts = d3.rollup(
+    data,
+    v => new Set(v.map(d => d.trackName)).size,
+    d => d.Nombre
+  );
+  const hoursPlayed = d3.rollup(
+    data,
+    v => d3.sum(v, d => d.msPlayed),
+    d => d.Nombre
+  );
 
+  const container = d3.select("#container");
+
+  /* Crear tabla en el HTML */
+  const tableDiv = container.append("div").style("flex-grow", 1);
+  const table = tableDiv.append("table").classed("table", true).style("margin", "auto");
 
   /* Encabezado de la tabla */
   table.append("thead")
     .append("tr")
     .selectAll("th")
-    .data(["Barrio", "Cantidad de contactos"])
+    .data(["", "Total artistas", "Total canciones", "Horas totales"])
     .enter()
     .append("th")
     .text(d => d);
@@ -16,12 +37,18 @@ const table = tableDiv.append("table").style("margin", "auto");
   /* Filas de la tabla */
   const rows = table.append("tbody")
     .selectAll("tr")
-    .data(conteoContactos)
+    .data(Array.from(artistCounts.keys()))
     .enter()
     .append("tr");
 
   /* Celdas de la tabla */
   rows.append("td")
-    .text(d => d.barrio);
+    .text(d => d);
   rows.append("td")
-    .text(d => d.cantidad);
+    .text(d => artistCounts.get(d));
+  rows.append("td")
+    .text(d => songCounts.get(d));
+  rows.append("td")
+    .text(d => Math.floor(hoursPlayed.get(d)));
+});
+
